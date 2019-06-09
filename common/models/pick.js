41b42c1300109480971ca9041d112299a6559fa7
Romain Rousseau <romain.rousseau5@gmail.com>
2019-06-09 21:42:36 +0200

    added ofUser endpoint to Picks

--- common/models/pick.js
+++ common/models/pick.js
@@ -2,4 +2,30 @@
 
 module.exports = function(Pick) {
 
+	Pick.ofUser = function(userId, cb) {
+		var filter = '{"where":{"userId":"' + userId + '"}}';
+
+	    Pick.find(JSON.parse(filter), function (err, instance) {
+	    	console.log(instance.length);
+	        cb(null, instance);
+	        console.log(instance);
+	    });
+	};
+
+	Pick.remoteMethod(
+	'ofUser', {
+            http: {
+                path: '/ofUser',
+                verb: 'get'
+            },
+            accepts: [
+                {arg: 'userId', type: 'number', http: {source: 'query'}},
+            ],
+            returns : {
+                arg: 'picks',
+                type: 'array'
+            }
+        }
+	)
+
 };
--- common/models/user.js
+++ common/models/user.js
@@ -1,5 +1,17 @@
 'use strict';
+let app = require('../../server/server');
 
 module.exports = function(User) {
+/*
+	User.calcPoints = function(userId, cb) {
 
+		var Picks = app.models.Picks;
+		var filter = '{"where":{"something":"value"}}';
+
+    	Picks.ofUser( userId, function (err, instance) {
+        	var response = "Name of coffee shop is " + instance.name;
+        	cb(null, response);
+        	console.log(response);
+    });
+  }*/
 };
